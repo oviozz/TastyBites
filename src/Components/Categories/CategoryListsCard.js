a8d57ddeb91b840de45de66dae4eef296628e5b4
oviozz <42685801+oviozz@users.noreply.github.com>
2023-08-21 22:24:11 -0700

    Update CategoryListsCard.js

--- src/Components/Categories/CategoryListsCard.js
+++ src/Components/Categories/CategoryListsCard.js
@@ -1,5 +1,4 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHeart} from "@fortawesome/free-solid-svg-icons";
+
 import {Link} from "react-router-dom";
 
 
@@ -7,7 +6,7 @@ function CategoryListsCard(props){
 
     return (
         <Link to={`/category/${props.title}`}>
-            <li key={props.index} id={props.title} className={"category--item"}>
+            <li key={props.index} id={props.title} className={"category--item"} style={props.styleIndex}>
                 <img className={"category--img"} src={props.image} alt={"props.alt"} />
                 <h2 className={"item--title"}>{props.title}</h2>
             </li>
