deec488af7a3a5ebdc693d80640a9811a45e37fe
oviozz <42685801+oviozz@users.noreply.github.com>
2023-08-21 22:24:29 -0700

    Update CategoryListsCard.js

--- src/Components/Categories/CategoryListsCard.js
+++ src/Components/Categories/CategoryListsCard.js
@@ -1,18 +1,88 @@
 
-import {Link} from "react-router-dom";
 
+import CategoryListsCard from "./CategoryListsCard";
+import "./CategoryList.css"
+import FoodRequestBody from "../LoadData/FoodRequestBody";
 
-function CategoryListsCard(props){
+function CategoryList(props){
 
-    return (
-        <Link to={`/category/${props.title}`}>
-            <li key={props.index} id={props.title} className={"category--item"} style={props.styleIndex}>
-                <img className={"category--img"} src={props.image} alt={"props.alt"} />
-                <h2 className={"item--title"}>{props.title}</h2>
-            </li>
-        </Link>
-    )
+    const categories = [
+        {
+            name: "Beef",
+            image: "https://www.themealdb.com/images/category/beef.png"
+        },
+        {
+            name: "Chicken",
+            image: "https://www.themealdb.com/images/category/chicken.png"
+        },
+        {
+            name: "Dessert",
+            image: "https://www.themealdb.com/images/category/dessert.png"
+        },
+        {
+            name: "Lamb",
+            image: "https://www.themealdb.com/images/category/lamb.png"
+        },
+        {
+            name: "Miscellaneous",
+            image: "https://www.themealdb.com/images/category/miscellaneous.png"
+        },
+        {
+            name: "Pasta",
+            image: "https://www.themealdb.com/images/category/pasta.png"
+        },
+        {
+            name: "Pork",
+            image: "https://www.themealdb.com/images/category/pork.png"
+        },
+        {
+            name: "Seafood",
+            image: "https://www.themealdb.com/images/category/seafood.png"
+        },
+        {
+            name: "Side",
+            image: "https://www.themealdb.com/images/category/side.png"
+        },
+        {
+            name: "Starter",
+            image: "https://www.themealdb.com/images/category/starter.png"
+        },
+        {
+            name: "Vegan",
+            image: "https://www.themealdb.com/images/category/vegan.png"
+        },
+        {
+            name: "Vegetarian",
+            image: "https://www.themealdb.com/images/category/vegetarian.png"
+        },
+    ];
+
+    const shuffledData = categories.sort(() => Math.random() - 0.5);
+
+    const categoryData = shuffledData.map((item, index) => {
+        return <CategoryListsCard index={index} image={item.image} title={item.name} styleIndex={{ animationDelay: `${index * 0.1}s` }}/>
+    })
+
+    const displaySelect = () => {
+        if (props.itemTitle){
+            return (
+                <>
+                    <h1 className={"component--pagetitle"}>{props.itemTitle}</h1>
+                    <FoodRequestBody category={props.itemTitle} method={"filter"}/>
+                </>
+            )
+        }else {
+            return (
+                <>
+                    <h1 className={"component--pagetitle"}>Categories</h1>
+                    <ul className={"category--container"}>
+                        {categoryData}
+                    </ul>
+                </>
+            )
+        }
+    }
+    return displaySelect();
 }
 
-
-export default CategoryListsCard;
+export default CategoryList;
